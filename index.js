e3ccbd56a9b4649b29cf5529ebd807c8a09a9030
GloriaAndonyaIT <gloriaandonyaa@gmail.com>
2025-02-28 21:20:42 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+
+const main = document.getElementById("main");
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Gloria Andonya is the champion";
+
+document.body.appendChild(newHeader);
